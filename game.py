b4cebf91062af13192e9f5f31ee604beaa2fd6a7
Arseniy <ArsikXX@gmail.com>
2021-01-31 20:57:01 +1000

    Анимация и прыжки - робят

--- game.py
+++ game.py
@@ -3,17 +3,18 @@ import sys
 
 # все переменные писать в этот блок. Желательно.
 
+size = width, height = 1280, 720
 running = True
-fps = 144
+fps = 60
 clock = pygame.time.Clock()
 replica_num = 0
 level_number = 0
 flag_dialog = 0
 
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Игра.екзе')
-    size = width, height = 1280, 720
     screen = pygame.display.set_mode(size)
 
 
@@ -26,7 +27,7 @@ if __name__ == '__main__':
         intro_text = ["ЗАСТАВКА", "",
                       "ТЕСТОВЫЙ ПРОБЕГ",
                       "!№;:?*()_+,",
-                      "Для продолжения нажмите любую клавишу"]
+                      "Для продолжения нажмите Enter"]
 
         screen.fill((0, 0, 0))
         font = pygame.font.Font(None, 30)
@@ -41,11 +42,11 @@ if __name__ == '__main__':
             screen.blit(string_rendered, intro_rect)
 
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for eevent in pygame.event.get():
+                if eevent.type == pygame.QUIT:
                     terminate()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
+                elif eevent.type == pygame.KEYDOWN or \
+                        eevent.type == pygame.MOUSEBUTTONDOWN:
                     return
             pygame.display.flip()
             clock.tick(fps)
@@ -60,15 +61,13 @@ class Player(pygame.sprite.Sprite):
                               pygame.image.load('sprites walk 3.png'), pygame.image.load('sprites walk 4.png'),
                               pygame.image.load('sprites walk 5.png'), pygame.image.load('sprites walk 4.png'),
                               pygame.image.load('sprites walk 3.png'), pygame.image.load('sprites walk 2-3.png'),
-                              pygame.image.load('sprites walk 2.png'),
-                              pygame.image.load('sprites walk 1.png')]
+                              pygame.image.load('sprites walk 2.png')]
         self.walk_left_gg = [pygame.image.load('sp w left 1.png'), pygame.image.load('sp w left 2.png'),
                              pygame.image.load('sp w left 2-3.png'), pygame.image.load('sp w left 3.png'),
                              pygame.image.load('sp w left 4.png'),
                              pygame.image.load('sp w left 5.png'), pygame.image.load('sp w left 4.png'),
                              pygame.image.load('sp w left 3.png'), pygame.image.load('sp w left 2-3.png'),
-                             pygame.image.load('sp w left 2.png'),
-                             pygame.image.load('sp w left 1.png')]
+                             pygame.image.load('sp w left 2.png')]
         self.stand = [pygame.image.load('stand 1.png'), pygame.image.load('stand 2.png'),
                       pygame.image.load('stand 3.png'), pygame.image.load('stand 4.png'),
                       pygame.image.load('stand 3.png'), pygame.image.load('stand 2.png')]
@@ -76,74 +75,75 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.index_right = 0
         self.i = 0
-        self.dy = 0
+        self.index_left = 0
+        self.counter = 0
+        self.speed_y = 0
+        self.timer = 0
+        self.step = 2.5
         self.jumped = False
 
-    def update(self, status):
-        if not self.jumped:
-            if status == 'walk_right':
-                self.image = self.walk_right_gg[self.i]
+    def update(self):
+        delta_x = 0
+        delta_y = 0
+        walk_cooldown = 6
+        wait_cooldown = 15
+
+
+        key = pygame.key.get_pressed()
+        # jump
+        if key[pygame.K_SPACE] and not self.jumped:
+            self.speed_y = -20
+            self.jumped = True
+        if not key[pygame.K_SPACE]:
+            self.jumped = False
+        # move & animate
+        if key[pygame.K_d] or key[pygame.K_RIGHT]:
+            delta_x += self.step
+            self.counter += 1
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index_right += 1
+                if self.index_right >= len(self.walk_right_gg):
+                    self.index_right = 0
+                self.image = self.walk_right_gg[self.index_right]
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
+            delta_x -= self.step
+            self.counter += 1
+            if self.counter > walk_cooldown:
+                self.counter = 0
+                self.index_left += 1
+                if self.index_left >= len(self.walk_left_gg):
+                    self.index_left = 0
+                self.image = self.walk_left_gg[self.index_left]
+        if not key[pygame.K_a] and not key[pygame.K_d]:
+            self.timer += 1
+            if self.timer > wait_cooldown:
+                self.timer = 0
                 self.i += 1
-                if self.i == len(self.walk_right_gg):
-                    self.i = 0
-                clock.tick(15)
-            if status == 'walk_left':
-                self.image = self.walk_left_gg[self.i]
-                self.i += 1
-                if self.i == len(self.walk_left_gg):
-                    self.i = 0
-                clock.tick(15)
-            if status == 'simple_stand':
                 if self.i >= len(self.stand):
                     self.i = 0
                 self.image = self.stand[self.i]
-                self.i += 1
-                if self.i == len(self.stand):
-                    self.i = 0
-                clock.tick(4)
-        if status == 'freeze':
-            self.image = self.stand[0]
-        screen.blit(self.image, self.rect)
 
-    def gravity_n_movement(self):
 
-        delta_x = 0
-        delta_y = 0
+        # phys
+        self.speed_y += 1
+        if self.speed_y > 10:
+            self.speed_y = 10
+        delta_y += self.speed_y
 
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and not self.jumped:
-            self.dy = -15
-            signal = 'freeze'
-            self.jumped = True
-        elif key[pygame.K_SPACE]:
-            self.jumped = False
-            signal = 'freeze'
+        if self.rect.bottom > height - 70:
+            self.rect.bottom = height - 70
 
-        if key[pygame.K_d] or key[pygame.K_RIGHT]:
-            delta_x += 10
-            signal = 'walk_right'
-        elif key[pygame.K_a] or key[pygame.K_LEFT]:
-            delta_x -= 10
-            signal = 'walk_left'
-        else:
-            signal = 'simple_stand'
-
-        self.dy += 1
-        if self.dy > 10:
-            self.dy = 10
-        delta_y += self.dy
-
-        if self.rect.bottom > height:
-            self.rect.bottom = height
-        self.update(signal)
         if self.rect.left < -130:
             self.rect.left = -130
         if self.rect.right > width + 130:
             self.rect.right = width + 130
         self.rect.x += delta_x
         self.rect.y += delta_y
-        #  screen.blit(self.image, self.rect)
+
+        screen.blit(self.image, self.rect)
 
 
 class World:
@@ -172,7 +172,7 @@ class World:
         screen.blit(self.background3, [0, 0])
 
 
-class Items():
+class Items:
     def __init__(self, *group):
         super().__init__(*group)
         self.corpses = pygame.image.load('tr.png')
@@ -208,7 +208,7 @@ class Items():
     def location1(self):
         screen.blit(self.charley, [310, 645])
         screen.blit(self.corpses, [0, 560])
-        if self.wire_it == False:
+        if not self.wire_it:
             screen.blit(self.wire, [745, 640])
 
     def location2(self):
@@ -233,23 +233,32 @@ class Items():
     def click(self, mouse_coordinates, location):  # то, где всё клацается
         if location == 0:
             if self.corpses_rect.collidepoint(mouse_coordinates):
-                return ['...', 'Как странно...Они должны защищать местных', 'Но получилось так, что они их убивают...',
-                        'Подонки.', 'Легли под шишек свыше, которые нам все входы и выходы перекрыли...',
-                        'И радуются...', 'Как и всегда это делали.', 'Не суть']
+                return ['хм...', 'Как странно...Они должны защищать местных',
+                        'Но получилось так, что они защищают ОТ местных'
+                        '...',
+                        'Подонки.', 'Легли под пиджаков, которые нам все входы и выходы перекрыли...',
+                        'А сами купаются в роскоши...', 'Как и всегда это делали.',
+                        'Так что удивляться тут нечему, надо двигаться дальше']
             if self.charley_rect.collidepoint(mouse_coordinates):
                 if self.door_it:
-                    return ['Держись, друг...', '...', 'Я доведу наше дело до конца...', 'end']
+                    return ['Держись, дружище...', '...', 'Я доведу наше дело до конца...', 'end']
                 else:
-                    return ['...', 'Будь бы ты сейчас жив, Чарли....', 'хах', '.....']
+                    return ['Эх, Чарли-Чарли...', 'Будь бы ты сейчас жив, Чарли....', 'хах', '.....']
             if self.wire_rect.collidepoint(mouse_coordinates):
-                if self.wire_it == False:
+                if not self.wire_it:
                     self.wire_it = True
-                    return ['Просто лежащий провод...', 'Думаю, его стоит подобрать...']
+                    return ['Просто лежащий провод...',
+                            'ПЭС-330ВвМ, "Фуджитсу Электроникс", тройная оплётка...',
+                            'Думаю, его стоит подобрать...']
+                else:
+                    return ['...']
             else:
                 return ['...']
+        if location == 1:
+            return ['...']
         if location == 2:
             if self.door_rect.collidepoint(mouse_coordinates):
-                if self.table_it and self.door_it == False:
+                if self.table_it and not self.door_it:
                     self.door_it = True
                     return ['A что, если....']
                 if self.door_it:
@@ -261,26 +270,27 @@ class Items():
 
             if self.table_rect.collidepoint(mouse_coordinates):
                 if self.table_it:
-                    return ['Панель состояния...', 'Судя по ней, сейчас всё в порядке, кроме одного блока...',
-                            'Когда это всё закончится, стану электриком, хах']
+                    return ['Генератор...', 'Судя по индикаторам, сейчас всё в порядке... ну кроме одного...',
+                            'Когда это всё закончится, устроюсь в "Юнайтед Пауерс" электриком на радостях, хах']
                 else:
-                    return ['Тут рядом где-то энергоблок...Должно быть, это панель состояния всей системы',
-                            'С ним всё в порядке, а вот с лифтом проблемы...',
-                            'Ибо она ничего хорошего не показывает']
+                    return ['Тут точно рядом есть щиток... А это, должно быть, генератор',
+                            'С ним всё в порядке, а вот с щитком лифта лифтом проблемы...',
+                            'Ибо все индикаторы красные']
             if self.box_rect.collidepoint(mouse_coordinates):
                 if self.oy_:
                     self.oy_ = False
-                    return ['....']
+                    return ['Надо рассмотреть это получше']
 
-                if self.box_it == False:
+                if not self.box_it:
                     if self.wire_it:
                         self.box_it = True
                         self.table_it = True
-                        return ['...', 'Я редко раньше работал с проводами...', 'Но я попробую...']
+                        return ['Проклятье', 'Щиток "Юнайтедов"... провод - "Фуджитсу"',
+                                'Хуже я уже точно не сделаю...']
 
                     else:
-                        return ['Разодранная панель для вызова лифта...', '''Надеюсь, они не додумались вывести сам лифт 
-                    из строя''']
+                        return ['Панель управления лифтом. Разбита...',
+                                '''Надеюсь, они не додумались вывести сам лифт из строя''']
                 else:
                     return ['Отныне я электрик...', 'Довольно странно, что они выдрали провод, а не просто выстрелили',
                             'Мы так несколько пути этим мразям перекрыли...' 'Хах....']
@@ -327,11 +337,11 @@ items = Items()
 start_screen()
 level = World()
 all_sprites = pygame.sprite.Group()
-violett = Player(x=25, y=320)
+violett = Player(x=25, y=height - 70)
 all_sprites.add(violett)
 dialog_window = DialogWindow()
 items_text = False
-defolt_text = ['...']
+default_text = ['...']
 test_text = ['Как-то раз я невзначай',
              'Сунул... а впрочем...',
              '']
@@ -341,6 +351,7 @@ forbidden_words = ('...', 'Тут ничего нет, лол',
 end = False  # переменная, которая подводит к концу игры. Она объвит функцию с титрами (да и не только)
 #  Появляется после выполнения всех заданий, когда Вайлет подходит к трупу
 while running:
+    clock.tick(fps)
     if violett.rect.x < 25 and level_number > 0:
         level_number -= 1
         violett.rect.x = 1100
@@ -360,22 +371,19 @@ while running:
         # закрывашка
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_e]:
-                flag_dialog = not flag_dialog  # == pose
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if flag_dialog:
-                if event.key in [pygame.K_l] and replica_num < len(defolt_text):
+                if event.button == 3 and replica_num < len(default_text):
                     replica_num += 1
-                if event.key in [pygame.K_j] and replica_num > 0:
+                if event.button == 2 and replica_num > 0:
                     replica_num -= 1
-                if replica_num == len(defolt_text):
+                if replica_num == len(default_text):
                     replica_num = 0
                     flag_dialog = False
                 if items_text:
                     items_text = False
 
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pygame.MOUSEBUTTONUP and not flag_dialog and event.button == 1:
             mouse_position = pygame.mouse.get_pos()
             result = items.click(mouse_position, level_number)
             if result[-1] == 'end':
@@ -383,16 +391,15 @@ while running:
                 end = True
             flag_dialog = True
             items_text = True
-            defolt_text = result
+            default_text = result
             result = '...'
 
-    if items_text == False and flag_dialog == 0:
-        defolt_text = test_text
-    violett.gravity_n_movement()
+    if not items_text and flag_dialog == 0:
+        default_text = test_text
+    violett.update()
 
     dialog_window.unknown_func(flag_dialog)
     if flag_dialog:
-        dialog_window.talk('Вайлет', defolt_text[replica_num])
+        dialog_window.talk('Вайлет', default_text[replica_num])
 
     pygame.display.update()
-    clock.tick(fps)
